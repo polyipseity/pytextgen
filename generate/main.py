6ba430f7c05603e967c8e1147e4bcabdc677e2a8
William So <polyipseity@gmail.com>
2022-10-18 00:12:48 +0800

    Improve argument parsing of 'tools.generate' using 'argparse'
    Signed-off-by: William So <polyipseity@gmail.com>

--- generate/main.py
+++ generate/main.py
@@ -1,38 +1,43 @@
+import argparse as _argparse
 import contextlib as _contextlib
 import logging as _logging
 import os as _os
 import pathlib as _pathlib
+import sys as _sys
 import typing as _typing
 
 from .. import globals as _globals
+from .. import version as _version
 from . import read as _read
 from . import write as _write
 
 
 def main(argv: _typing.Sequence[str]) -> None:
-    input: ParseArgvDict = parse_argv(argv)
+    args: Arguments = parse_argv(argv)
     writers: _typing.MutableSequence[_write.Writer] = []
-    path: _pathlib.PurePath
-    for path in input.paths:
+    input: _pathlib.PurePath
+    for input in args.inputs:
         try:
             file: _typing.TextIO = open(
-                path, mode='rt', **_globals.open_options)
+                input, mode='rt', **_globals.open_options)
         except OSError:
-            _logging.exception(f'Cannot open file: {path}')
+            _logging.exception(f'Cannot open file: {input}')
             continue
         except ValueError:
-            _logging.exception(f'Encoding error opening file: {path}')
+            _logging.exception(f'Encoding error opening file: {input}')
             continue
         with file:
             try:
                 ext: str
-                _, ext = _os.path.splitext(path)
+                _, ext = _os.path.splitext(input)
                 reader: _read.Reader = _read.Reader.registry[ext](
-                    path=path)
+                    path=input,
+                    timestamp=args.timestamp,
+                )
                 reader.read(file.read())
                 writers.extend(reader.pipe())
             except BaseException:
-                _logging.exception(f'Exception reading file: {path}')
+                _logging.exception(f'Exception reading file: {input}')
                 continue
 
     writer_stack: _contextlib.ExitStack = _contextlib.ExitStack().__enter__()
@@ -52,9 +57,46 @@ def main(argv: _typing.Sequence[str]) -> None:
 
 
 @_typing.final
-class ParseArgvDict(_typing.NamedTuple):
-    paths: _typing.Sequence[_pathlib.PurePath]
+class Arguments(_typing.NamedTuple):
+    inputs: _typing.Sequence[_pathlib.PurePath]
+    timestamp: bool
 
 
-def parse_argv(argv: _typing.Sequence[str]) -> ParseArgvDict:
-    return ParseArgvDict(paths=tuple(_pathlib.PurePath(path) for path in argv[1:]))
+def parse_argv(argv: _typing.Sequence[str]) -> Arguments | _typing.NoReturn:
+    prog0: str | None = _sys.modules[__name__].__package__
+    prog: str = prog0 if prog0 else __name__
+    del prog0
+
+    parser: _argparse.ArgumentParser = _argparse.ArgumentParser(
+        prog=f'python -m {prog}',
+        description='generate text from input',
+        add_help=True,
+        allow_abbrev=True,
+        exit_on_error=False,
+    )
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version=f'{prog} v{_version.version}',
+                        help='print version and exit',
+                        )
+    parser.add_argument('-t', '--timestamp',
+                        action='store_true',
+                        help='update or write timestamp (default)',
+                        dest='timestamp',
+                        )
+    parser.add_argument('-T', '--no-timestamp',
+                        action='store_false',
+                        help='do not update or write timestamp',
+                        dest='timestamp',
+                        )
+    parser.add_argument('inputs',
+                        action='store',
+                        nargs=_argparse.ONE_OR_MORE,
+                        type=_pathlib.PurePath,
+                        help='sequence of input(s) to read',
+                        )
+    input: _argparse.Namespace = parser.parse_args(argv[1:])
+    return Arguments(
+        inputs=tuple(input.inputs),
+        timestamp=input.timestamp,
+    )
