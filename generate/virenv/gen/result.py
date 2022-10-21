76c715672f8310e882e396775d37e699e93c42ea
William So <polyipseity@gmail.com>
2022-10-21 18:25:00 +0800

    Generalize 'Results' in 'result.py' to 'TypedTuple' in 'util.py'
    Signed-off-by: William So <polyipseity@gmail.com>

--- generate/virenv/gen/result.py
+++ generate/virenv/gen/result.py
@@ -1,24 +1,14 @@
 import typing as _typing
-from .. import util as _util
+from .... import util as _util
+from .. import util as _util2
 
 
 @_typing.final
 class Result(_typing.NamedTuple):
-    location: _util.Location
+    location: _util2.Location
     text: str
 
 
 @_typing.final
-class Results(tuple[Result, ...]):
-    @_typing.overload
-    def __new__(cls: type[_typing.Self], iterable: _typing.Iterable[Result], /) -> _typing.Self:
-        ...
-
-    @_typing.overload
-    def __new__(cls: type[_typing.Self], *items: Result) -> _typing.Self:
-        ...
-
-    def __new__(cls: type[_typing.Self], *items: _typing.Iterable[Result] | Result) -> _typing.Self:
-        if len(items) == 1 and not isinstance(items[0], Result):
-            return super().__new__(cls, items[0])
-        return super().__new__(cls, _typing.cast(_typing.Iterable[Result], items))
+class Results(_util.TypedTuple[Result], t_type=Result):
+    pass
--- util.py
+++ util.py
@@ -2,3 +2,29 @@ import os as _os
 import typing as _typing
 
 StrPath: _typing.TypeAlias = str | _os.PathLike[str]
+
+_T = _typing.TypeVar('_T')
+
+
+class TypedTuple(_typing.Generic[_T], tuple[_T, ...]):
+    __t_type: type[_T]
+
+    def __init_subclass__(cls, t_type: type[_T], **kwargs: _typing.Any) -> None:
+        cls.__t_type = t_type
+        return super().__init_subclass__(**kwargs)
+
+    @_typing.overload
+    def __new__(cls: type[_typing.Self], iterable: _typing.Iterable[_T], /) -> _typing.Self:
+        ...
+
+    @_typing.overload
+    def __new__(cls: type[_typing.Self], *items: _T) -> _typing.Self:
+        ...
+
+    def __new__(cls: type[_typing.Self], *items: _typing.Iterable[_T] | _T) -> _typing.Self:
+        if len(items) == 1 and not isinstance(items[0], cls.__t_type):
+            return super().__new__(cls, _typing.cast(_typing.Iterable[_T], items[0]))
+        return super().__new__(cls, _typing.cast(_typing.Iterable[_T], items))
+
+    def __repr__(self: _typing.Self) -> str:
+        return type(self).__qualname__ + super().__repr__()
