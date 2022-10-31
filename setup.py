d16add37b6a4d9ecc74cc96e39a8f0b86e65cf9a
William So <polyipseity@gmail.com>
2022-10-31 20:02:10 +0800

    Rename name from 'tools' to 'notes-tools'
    Signed-off-by: William So <polyipseity@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools as _setuptools
 import version as _version
 
 _setuptools.setup(
-    name='tools',
+    name='notes-tools',
     version=str(_version.version),
     install_requires=(
         'regex',
