02246179fbe19a279f957df8b6ea660148367451
William So <polyipseity@gmail.com>
2022-12-03 08:34:57 +0800

    Bump version from v1.13.1 to v1.13.2
    Signed-off-by: William So <polyipseity@gmail.com>

--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 import distutils.version as _distutils_version
 
-version: _distutils_version.Version = _distutils_version.StrictVersion("1.13.1")
+version: _distutils_version.Version = _distutils_version.StrictVersion("1.13.2")
