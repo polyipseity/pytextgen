432c7c46a2328b700bf38096cc867949d6b164ca
William So <polyipseity@gmail.com>
2023-01-31 22:16:28 +0800

    v2.1.1
    Signed-off-by: William So <polyipseity@gmail.com>

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: UTF-8 -*-
 import distutils.version as _distutils_version
 
-version: _distutils_version.Version = _distutils_version.StrictVersion("2.1.0")
+version: _distutils_version.Version = _distutils_version.StrictVersion("2.1.1")
