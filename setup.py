6db350870d00bd9e87b6dd9ceb8638c8baf2e0df
William So <polyipseity@gmail.com>
2023-03-01 09:56:23 +0800

    Rename project from `notes-tools` to `pytextgen`
    Signed-off-by: William So <polyipseity@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools as _setuptools
 import version as _version
 
 _setuptools.setup(
-    name="notes-tools",
+    name="pytextgen",
     version=str(_version.version),
     install_requires=("regex",),
 )
