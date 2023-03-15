e7bdf21a277d5271caef130df78b031b10d2d30f
William So <polyipseity@gmail.com>
2023-03-15 12:58:47 +0800

    Remove unnecessary typings
    Signed-off-by: William So <polyipseity@gmail.com>

--- clear/__main__.py
+++ clear/__main__.py
@@ -1,9 +1,8 @@
 # -*- coding: UTF-8 -*-
-import argparse as _argparse
 import asyncio as _asyncio
 import sys as _sys
 from . import main as _main
 
 if __name__ == "__main__":
-    entry: _argparse.Namespace = _main.parser().parse_args(_sys.argv[1:])
+    entry = _main.parser().parse_args(_sys.argv[1:])
     _asyncio.run(entry.invoke(entry))
