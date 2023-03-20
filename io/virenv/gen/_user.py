5496aff24b5f12726cc7fdd18c6e824b782424f0
William So <polyipseity@gmail.com>
2023-03-20 16:15:53 +0800

    Fix `rows_to_table()` for empty strings in table
    Signed-off-by: William So <polyipseity@gmail.com>

--- io/virenv/gen/_user.py
+++ io/virenv/gen/_user.py
@@ -450,9 +450,9 @@ def rows_to_table(
 ) -> str:
     names0 = _IterSeq(iter(names))
     lf = "\n"
-    return f"""{' | '.join(name if isinstance(name, str) else name[0] for name in names0)}
-{'|'.join(_TABLE_ALIGNS['default' if isinstance(name, str) else name[1]] for name in names0)}
-{lf.join(' | '.join(map(str, values(row))) for row in rows)}"""
+    return f"""| {' | '.join(name if isinstance(name, str) else name[0] for name in names0)} |
+| {'|'.join(_TABLE_ALIGNS['default' if isinstance(name, str) else name[1]] for name in names0)} |
+| {lf.join(' | '.join(map(str, values(row))) for row in rows)} |"""
 
 
 def two_columns_to_code(
