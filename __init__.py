fbf0ef5d6bb8a73beebaaca93ad336b8e976618b
William So <polyipseity@gmail.com>
2023-10-11 08:05:23 +0800

    6.3.1
    Signed-off-by: William So <polyipseity@gmail.com>

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ class _OpenOptions(_TDict):
 
 # update `pyproject.toml`
 NAME = "pytextgen"
-VERSION = _SVer("6.3.0")
+VERSION = _SVer("6.3.1")
 
 FLASHCARD_EASE_DEFAULT = 250
 FLASHCARD_STATES_FORMAT = "<!--SR:{states}-->"
